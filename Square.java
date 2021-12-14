2aca7e759d8c938a6146d9d5bb687abd4fc5bfa1
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 20:10:21 -0500

    Update Square.java

--- Square.java
+++ Square.java
@@ -1,4 +1,3 @@
-//Add class comment and @ tags
 /**
  * This class models a square.
  *
@@ -8,28 +7,17 @@
 
 public class Square extends Parallelogram
 {
-    /**
-     * Constructor that takes an integer side length
-     * @param side of Square
-     */
+
     public Square(int side) 
     {
         super(side, side);
     }
   
-    /**
-     * Abstract method from the superclass that takes no param and returns area of square.
-     * @return area of Square
-     */    
     public int getArea() 
     {
         return getArea(getLength(), getLength()); // calls helper recursive method to return area
     }
-  
-    /**
-     * Computes area of square.
-     * @return area of Square
-     */
+
     private int getArea(int side, int n) 
     {
         if(side <= 0 || n <= 0) // side or n is less than 0, return 0
