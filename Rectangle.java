1c0c63b05234005af2263bcc33e8567aff655a50
Austin <69608231+walley1@users.noreply.github.com>
2021-12-13 20:09:27 -0500

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,3 @@
-//Add class comment and @ tags
 /** 
  * This class models a rectangle.
  *
@@ -7,29 +6,17 @@
  */
 public class Rectangle extends Parallelogram 
 {
-    /**
-     * Constructor that takes length and width as param.
-     * @param length of Rectangle
-     * @param width of Rectangle
-     */
+
     public Rectangle(int length, int width) 
     {
         super(length, width);
     }
-    
-    /**
-     * Abstract method from the superclass that takes no param and returns area of rectangle.
-     * @return area of Rectangle
-     */    
+       
     public int getArea() 
     {
         return getArea(getLength(), getWidth()); 
     }
     
-    /**
-     * Computes area of rectangle.
-     * @return area of Rectangle
-     */
     private int getArea(int length, int width) 
     {
         if (width <= 0 || length <= 0)
